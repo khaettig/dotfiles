1660bbcf593b039b2f4182c7491c00e6e9be712a
Kevin Hättig <haettigk@cs.uni-freiburg.de>
2020-02-12 16:26:01 +0100

    Initial commit.

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+install() {
+    FILE=$1
+    if test -f ~/.$FILE; then
+        echo "Moving existing file ~/.$FILE to ~/.${FILE}_old"
+        mv ~/.$FILE ~/.${FILE}_old
+    fi
+
+    echo "Creating soft link for .$FILE"
+    ln -s $(pwd)/$FILE ~/.$FILE
+}
+
+install vimrc
+install bashrc
+install tmux.conf
