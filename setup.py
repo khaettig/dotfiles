9a9485c2a626b8aa7184e5cae299bc3a4f87104d
Kevin Hättig <kevin.haettig@posteo.de>
2020-08-23 14:00:52 +0200

    Set python linelength to 88

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
 
-import os
 from pathlib import Path
 
 
 def install(source, link):
     if link.exists():
-        backup = link.with_suffix('.old')
-        print(f'Moving existing file to {backup}')
+        backup = link.with_suffix(".old")
+        print(f"Moving existing file to {backup}")
         link.rename(backup)
 
-    print(f'Creating soft link for {source}')
+    print(f"Creating soft link for {source}")
     link.symlink_to(source)
 
 
-if __name__ == '__main__':
-    files = [ 
-        ('init.vim', '~/.config/nvim/init.vim'),
-        ('bashrc', '~/.bashrc'),
-        ('pylintrc', '~/.pylintrc'),
-        ('pdbrc.py', '~/.pdbrc.py'),
-        ('tmux.conf', '~/.tmux.conf')]
+def main():
+    files = [
+        ("init.vim", "~/.config/nvim/init.vim"),
+        ("bashrc", "~/.bashrc"),
+        ("pylintrc", "~/.pylintrc"),
+        ("pdbrc.py", "~/.pdbrc.py"),
+        ("tmux.conf", "~/.tmux.conf"),
+        ("pycodestyle", "~/.config/pycodestyle"),
+    ]
 
     for source, link in files:
         install(Path.cwd() / source, Path(link).expanduser())
+
+
+if __name__ == "__main__":
+    main()
