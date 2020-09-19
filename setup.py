ce1c0404f537f64626b76f99f5c2b7c944547030
Kevin Hättig <kevin.haettig@posteo.de>
2020-09-19 10:52:08 +0200

    Add nvim installation to setup

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
+import os
+import stat
 from pathlib import Path
+
+import requests
+
+NVIM_URL = "https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage"
+NVIM_INSTALL = f"{Path.home()}/.local/bin/nvim"
+
+
+def download_file(url, destination):
+    print(f"Downloading {url} to {destination}")
+    request = requests.get(url)
+    with open(destination, "wb") as f:
+        f.write(request.content)
+
+
+def make_executable(file_name):
+    os.chmod(file_name, os.stat(file_name).st_mode | stat.S_IEXEC)
 
 
 def install(source, link):
@@ -11,6 +29,14 @@ def install(source, link):
 
     print(f"Creating soft link for {source}")
     link.symlink_to(source)
+
+
+def install_nvim_if_not_installed():
+    if Path(NVIM_INSTALL).is_file():
+        return
+    print("Installing nvim")
+    download_file(NVIM_URL, NVIM_INSTALL)
+    make_executable(NVIM_INSTALL)
 
 
 def main():
@@ -25,6 +51,7 @@ def main():
 
     for source, link in files:
         install(Path.cwd() / source, Path(link).expanduser())
+    install_nvim_if_not_installed()
 
 
 if __name__ == "__main__":
