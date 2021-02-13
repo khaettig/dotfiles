2d2fa3572bd3506945d104ba4de38966659c85d4
Kevin Hättig <kevin.haettig@posteo.de>
2021-02-13 18:51:39 +0100

    Fix smaller problems in kvim.sh

--- .scripts/kvim.sh
+++ .scripts/kvim.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# curl -sSL https://raw.githubusercontent.com/khaettig/dotfiles/master/.scripts/kvim.sh | sh
+
 if [ ! -f ~/.local/bin/nvim ]; then
   echo "Downloading neovim"
   mkdir -p ~/.local/bin
-  curl https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output ~/.local/bin/nvim 
+  curl -sSL https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o ~/.local/bin/nvim
 fi
 
 if [ ! -f ~/.config/kinit.vim ]; then
   echo "Downloading init.vim"
   mkdir -p ~/.config
-  curl https://raw.githubusercontent.com/khaettig/dotfiles/master/.config/nvim/init.vim --output ~/.config/kinit.vim 
+  curl -sSL https://raw.githubusercontent.com/khaettig/dotfiles/master/.config/nvim/init.vim -o ~/.config/kinit.vim
 fi
 
-nvim -S ~/.config/kinit.vim
+chmod +x ~/.local/bin/nvim
+~/.local/bin/nvim -S ~/.config/kinit.vim
