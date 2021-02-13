e44a7c9aca779ef1ec49c5f4f4c675d85e70976f
Kevin Hättig <kevin.haettig@posteo.de>
2021-02-13 18:29:22 +0100

    Use curl instead of wget

--- .scripts/kvim.sh
+++ .scripts/kvim.sh
@@ -2,12 +2,14 @@
 
 if [ ! -f ~/.local/bin/nvim ]; then
   echo "Downloading neovim"
-  wget -O ~/.local/bin/nvim https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+  mkdir -p ~/.local/bin
+  curl https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output ~/.local/bin/nvim 
 fi
 
 if [ ! -f ~/.config/kinit.vim ]; then
   echo "Downloading init.vim"
-  wget -O ~/.config/kinit.vim https://raw.githubusercontent.com/khaettig/dotfiles/master/.config/nvim/init.vim 
+  mkdir -p ~/.config
+  curl https://raw.githubusercontent.com/khaettig/dotfiles/master/.config/nvim/init.vim --output ~/.config/kinit.vim 
 fi
 
 nvim -S ~/.config/kinit.vim
