979a664c872743ee4096cc0f06691f35ca1a8821
Kevin Hättig <kevin.haettig@posteo.de>
2021-03-16 16:42:37 +0100

    Add hotkey and display to easily mute microphone

---
+++ .scripts/get_microphone_state.py
@@ -0,0 +1,12 @@
+from subprocess import run, PIPE
+
+
+def main():
+    raw = run(["amixer", "-c", "1"], stdout=PIPE).stdout.decode("utf-8")
+
+    if "[off]" in raw:
+        print("  MUTED ⚠️  ")
+
+
+if __name__ == "__main__":
+    main()
