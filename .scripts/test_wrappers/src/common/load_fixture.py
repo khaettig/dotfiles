093c89eb804f70d11726b0a65ebb30aac4cd8425
Kevin Hättig <kevin.haettig@posteo.de>
2021-03-17 22:06:46 +0100

    Don't save empty outputs explicitly

--- .scripts/test_wrappers/src/common/load_fixture.py
+++ .scripts/test_wrappers/src/common/load_fixture.py
@@ -1,10 +1,20 @@
+from pathlib import Path
+
+
 def get_fixture_prefix(runner_name, fixture_name):
-    return f"src/{runner_name}/fixtures/{fixture_name}"
+    return f"src/{runner_name}/fixtures/{fixture_name}_"
+
+
+def load_file_if_exists(path):
+    if not Path(path).is_file():
+        return ""
+    with open(path) as f:
+        return f.read()
 
 
 def load_fixture(runner_name, fixture_name):
-    with open(get_fixture_prefix(runner_name, fixture_name) + "_stdout.json") as f:
-        stdout = f.read()
-    with open(get_fixture_prefix(runner_name, fixture_name) + "_stderr.json") as f:
-        stderr = f.read()
-    return stdout, stderr
+    prefix = get_fixture_prefix(runner_name, fixture_name)
+    return (
+        load_file_if_exists(prefix + "stdout.json"),
+        load_file_if_exists(prefix + "stderr.json"),
+    )
