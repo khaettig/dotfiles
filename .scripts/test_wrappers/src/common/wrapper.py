3e26b0a18b4c494da3fb7f97b6cf13566481a712
Kevin Hättig <kevin.haettig@posteo.de>
2021-03-17 20:55:22 +0100

    Cleanup base wrapper class

--- .scripts/test_wrappers/src/common/wrapper.py
+++ .scripts/test_wrappers/src/common/wrapper.py
@@ -1,50 +1,62 @@
 import sys
 import time
 import argparse
-from subprocess import run, PIPE
+from subprocess import run, PIPE, CalledProcessError
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--print_raw", action="store_true")
+    return parser.parse_known_args()
+
+
+def render_messages(messages):
+    for message in messages:
+        message.render()
+
+
+def render_summary(summary, duration):
+    print(f"{summary} in {duration:.2f}s")
+
+
+def print_raw(stdout, stderr, returncode):
+    print("Output (stdout):")
+    print(stdout)
+    print("Errors (stderr):")
+    print(stderr)
+    print(f"Returncode: {returncode}")
 
 
 class Wrapper:
+    command = []
+
     def run(self):
-        wrapper_args, command_args = self.parse_arguments()
+        wrapper_args, command_args = parse_arguments()
 
         time_before = time.time()
         stdout, stderr, returncode = self.run_command(command_args)
         duration = time.time() - time_before
 
         if wrapper_args.print_raw:
-            self.print_raw(stdout, stderr, returncode)
-            exit(0)
+            print_raw(stdout, stderr, returncode)
+            sys.exit(returncode)
 
         if stderr:
             print(stderr)
-            exit(1)
+            sys.exit(returncode)
 
         messages, summary = self.parse(stdout=stdout, stderr=stderr)
-        self.render_messages(messages)
-        self.render_summary(summary, duration)
+        render_messages(messages)
+        render_summary(summary, duration)
 
-        exit(returncode)
+        sys.exit(returncode)
 
-    def parse_arguments(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--print_raw", action="store_true")
-        return parser.parse_known_args()
+    def parse(self, stdout, stderr):
+        raise NotImplementedError
 
     def run_command(self, options):
-        call = run(self.command + options, stdout=PIPE, stderr=PIPE)
+        try:
+            call = run(self.command + options, stdout=PIPE, stderr=PIPE, check=True)
+        except CalledProcessError:
+            pass  # stderr is passed anyway
         return call.stdout.decode("utf-8"), call.stderr.decode("utf-8"), call.returncode
-
-    def render_messages(self, messages):
-        for message in messages:
-            message.render()
-
-    def render_summary(self, summary, duration):
-        print(f"{summary} in {duration:.2f}s")
-
-    def print_raw(self, stdout, stderr, returncode):
-        print("Output (stdout):")
-        print(stdout)
-        print("Errors (stderr):")
-        print(stderr)
-        print(f"Returncode: {returncode}")
