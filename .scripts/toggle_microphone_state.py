afcf3e65c148f05f0a0067fff53b363ae381d8bd
Kevin Hättig <kevin.haettig@posteo.de>
2021-03-18 16:39:28 +0100

    Add script to toggle microphone state

---
+++ .scripts/toggle_microphone_state.py
@@ -0,0 +1,44 @@
+from subprocess import run, PIPE
+
+ON = """
+                    ______   __    __
+                   /      \ /  \  /  |
+                  /$$$$$$  |$$  \ $$ |
+                  $$ |  $$ |$$$  \$$ |
+                  $$ |  $$ |$$$$  $$ |
+                  $$ |  $$ |$$ $$ $$ |
+                  $$ \__$$ |$$ |$$$$ |
+                  $$    $$/ $$ | $$$ |
+                   $$$$$$/  $$/   $$/
+
+"""
+
+OFF = """
+                ______   ________  ________
+               /      \ /        |/        |
+              /$$$$$$  |$$$$$$$$/ $$$$$$$$/
+              $$ |  $$ |$$ |__    $$ |__
+              $$ |  $$ |$$    |   $$    |
+              $$ |  $$ |$$$$$/    $$$$$/
+              $$ \__$$ |$$ |      $$ |
+              $$    $$/ $$ |      $$ |
+               $$$$$$/  $$/       $$/
+
+"""
+
+
+def main():
+    raw = run(
+        ["amixer", "-c", "1", "sset", "Mic", "toggle"], stdout=PIPE, check=True
+    ).stdout.decode("utf-8")
+
+    if "[off]" in raw:
+        message = ["critical", OFF]
+    else:
+        message = ["normal", ON]
+
+    run(["notify-send", "-t", "2000", "-u", *message], check=True)
+
+
+if __name__ == "__main__":
+    main()
