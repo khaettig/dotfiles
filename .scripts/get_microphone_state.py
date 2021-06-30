d20932eda89e4144ff4e3d1253c0d58c2362f54d
Kevin Hättig <kevin.haettig@posteo.de>
2021-06-30 11:08:29 +0200

    Fix microphone mute problem

--- .scripts/get_microphone_state.py
+++ .scripts/get_microphone_state.py
@@ -2,7 +2,7 @@ from subprocess import run, PIPE
 
 
 def main():
-    raw = run(["amixer", "-c", "2"], stdout=PIPE).stdout.decode("utf-8")
+    raw = run(["amixer", "-c", "1"], stdout=PIPE).stdout.decode("utf-8")
 
     if "[off]" in raw:
         print("  MUTED ⚠️  ")
