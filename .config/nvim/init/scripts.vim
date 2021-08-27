a22144062913fa992efa5a16f89b94fc941cef32
Kevin Hättig <kevin.haettig@posteo.de>
2021-08-27 21:50:17 +0200

    Add :Wait/:Waiting gtd command

--- .config/nvim/init/scripts.vim
+++ .config/nvim/init/scripts.vim
@@ -3,10 +3,12 @@
 
 :source ~/.scripts/gtd.py
 :command! Archive :py3 gtd_archive()
-:command! Arc :py3 gtd_archive()
+:command! Arch :py3 gtd_archive()
 :command! Project :py3 gtd_project()
-:command! Pro :py3 gtd_project()
+:command! Proj :py3 gtd_project()
 :command! Someday :py3 gtd_someday()
-:command! Som :py3 gtd_someday()
+:command! Some :py3 gtd_someday()
+:command! Waiting :py3 gtd_waiting()
+:command! Wait :py3 gtd_waiting()
 
 :command! -nargs=1 Rebase :!python3 ~/.scripts/rebase.py <f-args>
--- .scripts/gtd.py
+++ .scripts/gtd.py
@@ -1,12 +1,14 @@
 python3 << endpython
-
-import vim
 import os
+import vim
+
 
 NOTES_DIR = "Notes/"
 ARCHIVE_DIR = "Archive/"
 PROJECTS_DIR = "Projects/"
 SOMEDAY_DIR = "Someday/"
+WAITING_DIR = "Waiting/"
+
 
 def move_to_dir(dir_name):
     paths = vim.current.buffer.name.split(NOTES_DIR)
@@ -15,13 +17,21 @@ def move_to_dir(dir_name):
     os.rename(vim.current.buffer.name, new_path)
     vim.current.buffer.name = new_path
 
+
 def gtd_archive():
     move_to_dir(ARCHIVE_DIR)
+
 
 def gtd_project():
     move_to_dir(PROJECTS_DIR)
 
+
 def gtd_someday():
     move_to_dir(SOMEDAY_DIR)
+
+
+def gtd_waiting():
+    move_to_dir(WAITING_DIR)
+
 
 endpython
