b46cd97ccbb196ba733d9e1c52a03b183c252ac2
Kevin Hättig <kevin.haettig@posteo.de>
2021-09-23 22:30:09 +0200

    Remove deprecated pyls and add pylsp

--- .config/nvim/init/plugins/lsp.lua
+++ .config/nvim/init/plugins/lsp.lua
@@ -35,9 +35,10 @@ local on_attach = function(client, bufnr)
   end
 end
 
+
 -- Use a loop to conveniently both setup defined servers 
 -- and map buffer local keybindings when the language server attaches
-local servers = { "pyls", "tsserver"}
+local servers = { "pylsp", "tsserver"}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
