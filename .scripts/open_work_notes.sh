750e4e0146090fec225eff56b4eb3f824e4b3f33
Kevin Hättig <kevin.haettig@posteo.de>
2022-03-23 10:25:31 +0100

    Fix work notes split inside tmux

--- .scripts/open_work_notes.sh
+++ .scripts/open_work_notes.sh
@@ -2,7 +2,7 @@
 
 cd ~/WorkNotes
 if [[ -n "$TMUX" ]]; then
-  nvim -o Incoming.md NextActions.md
+  nvim -O Incoming.md NextActions.md
 else
   tmux new-session -A -s work nvim -O Incoming.md NextActions.md
 fi
