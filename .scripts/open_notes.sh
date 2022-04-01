e3d8497d8698edf6058dc209e9d0eba564cea1c2
Kevin Hättig <kevin.haettig@posteo.de>
2022-04-01 19:06:04 +0200

    Show dailies in open_notes

--- .scripts/open_notes.sh
+++ .scripts/open_notes.sh
@@ -2,7 +2,7 @@
 
 cd ~/Notes
 if [[ -n "$TMUX" ]]; then
-  nvim -o Incoming.md System/NextActions.md
+  nvim Incoming.md -c "sp System/Review_Daily.md" -c "vsp System/NextActions.md" -c "execute \"normal! \<C-W>L\"" -c "execute \"normal! \<C-W>h\""
 else
-  tmux new-session -A -s notes nvim -O Incoming.md System/NextActions.md
+  tmux new-session -A -s notes nvim Incoming.md -c "sp System/Review_Daily.md" -c "vsp System/NextActions.md" -c "execute \"normal! \<C-W>L\"" -c "execute \"normal! \<C-W>h\""
 fi
