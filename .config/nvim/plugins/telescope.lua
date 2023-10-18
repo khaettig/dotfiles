c5d195493dc7355e1de9298d0deaa8e8f29e2dc5
Kevin Hättig <kevin.haettig@posteo.de>
2023-10-18 15:22:20 +0200

    Add command to grep for current file name

--- .config/nvim/plugins/telescope.lua
+++ .config/nvim/plugins/telescope.lua
@@ -5,6 +5,7 @@ require('telescope').setup{
 }
 require('telescope').load_extension('fzf')
 
+vim.api.nvim_set_keymap('n', '<leader>ee', '<cmd>Telescope resume<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>ef', '<cmd>Telescope find_files<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>eg', '<cmd>Telescope live_grep<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>en', '<cmd>Telescope live_grep cwd=~/.config/nvim<CR>', {})
@@ -14,4 +15,5 @@ vim.api.nvim_set_keymap('n', '<leader>ec', '<cmd>Telescope git_commits<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>es', '<cmd>Telescope git_stash<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>eq', '<cmd>Telescope quickfix<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>em', '<cmd>Telescope marks<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>eh', '<cmd>lua require("telescope.builtin").live_grep({ default_text=vim.fn.expand("%:t") })<CR>', {})
 vim.api.nvim_set_keymap('v', '<leader>eg', 'y<ESC>:Telescope live_grep default_text=<c-r>0<CR>', {})
