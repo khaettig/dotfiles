5c8c009cd102a30315f5d98ce97f2993978b3814
Kevin Hättig <kevin.haettig@posteo.de>
2023-10-30 23:37:40 +0100

    Clean up md_goto_link

--- .config/nvim/lua/functions/md_goto_link.lua
+++ .config/nvim/lua/functions/md_goto_link.lua
@@ -1,45 +1,62 @@
 local system = vim.system
 
+local projects_folder = "Projects/"
+local project_template = "System/ProjectTemplate.md"
+
+local function handle_command(type, link)
+    if type == "file" then
+        system({ "xdg-open", vim.fn.expand(link) })
+        return
+    elseif type == "cmd" then
+        system({ link })
+        return
+    elseif type == "url" then
+        system({ "google-chrome", "--new-window", link })
+        return
+    elseif type == "ff" then
+        system({ "firefox", "--new-window", link })
+        return
+    elseif type == "yt" then
+        system({ "firefox", "--new-window", "youtube.com/watch?v=" .. link })
+        return
+    end
+end
+
+local function handle_system_link(link)
+    vim.cmd.edit(string.sub(link, 2) .. ".md")
+end
+
+local function handle_project_link(link)
+    local projects = {}
+    for _, file in pairs(vim.fn.globpath(projects_folder, "*.md", 0, 1)) do
+        projects[string.match(file, ".*_(.*)%.md")] = file
+    end
+
+    if projects[link] then
+        vim.cmd.edit(projects_folder .. projects[link])
+    else
+        vim.cmd.edit(projects_folder .. os.date("%Y-%m-%d") .. "_" .. link .. ".md")
+        vim.cmd.read(project_template)
+        vim.fn.feedkeys("kddo")
+    end
+end
+
 return function()
     local line = vim.api.nvim_get_current_line()
     local match = string.match(line, "%[(.*)%]")
 
     if not match then return end
 
-    for type, link in string.gmatch(match, "(%w*):(.*)") do
-        if type == "file" then
-            system({ "xdg-open", vim.fn.expand(link) })
-            return
-        elseif type == "cmd" then
-            system({ link })
-            return
-        elseif type == "url" then
-            system({ "google-chrome", "--new-window", link })
-            return
-        elseif type == "ff" then
-            system({ "firefox", "--new-window", link })
-            return
-        elseif type == "yt" then
-            system({ "firefox", "--new-window", "youtube.com/watch?v=" .. link })
-            return
-        end
-    end
-
-    if string.sub(match, 1, 1) == "/" then
-        vim.cmd.edit(string.sub(match, 2) .. ".md")
+    local type, link = string.match(match, "(%w*):(.*)")
+    if type then
+        handle_command(type, link)
         return
     end
 
-    local projects = {}
-    for _, file in pairs(vim.fn.globpath("Projects", "*.md", 0, 1)) do
-        projects[string.match(file, ".*_(.*)%.md")] = file
+    if string.sub(match, 1, 1) == "/" then
+        handle_system_link(match)
+        return
     end
 
-    if projects[match] then
-        vim.cmd.edit("Projects/" .. projects[match])
-    else
-        vim.cmd.edit("Projects/" .. os.date("%Y-%m-%d") .. "_" .. match .. ".md")
-        vim.cmd.read("System/ProjectTemplate.md")
-        vim.fn.feedkeys("kddo")
-    end
+    handle_project_link(match)
 end
