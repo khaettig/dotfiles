8b07a83fc4bf73357f68b3a9d2c8118f8b36bbb7
Kevin Hättig <kevin.haettig@posteo.de>
2023-11-04 11:37:09 +0100

    Don't auto trigger start/stop for dap ui

--- .config/nvim/lua/plugins/_nvim-dap.lua
+++ .config/nvim/lua/plugins/_nvim-dap.lua
@@ -1,6 +1,5 @@
 local dap = require("dap")
 local dapui = require("dapui")
-local colors = require("colors")
 
 dapui.setup({
     layouts = {
@@ -23,16 +22,6 @@ dapui.setup({
         },
     }
 })
-
-dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
-end
 
 require("dap-python").setup("python3")
 require('dap-python').test_runner = 'pytest'
